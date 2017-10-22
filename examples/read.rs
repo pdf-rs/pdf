2e671a84670d3144076cdd9bcb6218fa7020e1e7
Sebastian Köln <sebk@rynx.org>
2017-10-22 19:13:15 +0200

    print correct time

--- examples/read.rs
+++ examples/read.rs
@@ -4,15 +4,13 @@ use std::env::args;
 use std::time::SystemTime;
 
 use pdf::file::File;
-use pdf::object::*;
-use pdf::parser::parse;
 use pdf::print_err;
 
 fn main() {
     let path = args().nth(1).expect("no file given");
     println!("read: {}", path);
-    let file = File::<Vec<u8>>::open(&path).unwrap_or_else(|e| print_err(e));
     let now = SystemTime::now();
+    let file = File::<Vec<u8>>::open(&path).unwrap_or_else(|e| print_err(e));
     
     let num_pages = file.get_root().pages.count;
     for i in 0..num_pages {
