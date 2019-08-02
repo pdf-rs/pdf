b7069e13a1bbf31b6e2fda0a73a1c14d1f060142
Sebastian Köln <sebk@rynx.org>
2019-08-02 20:17:42 +0300

    remove some debug print

--- pdf/src/object/stream.rs
+++ pdf/src/object/stream.rs
@@ -23,7 +23,6 @@ pub struct Stream<I: Object=()> {
 impl<I: Object + fmt::Debug> Stream<I> {
     pub fn data(&self) -> Result<&[u8]> {
         self.decoded.get_or_try_init(|| {
-            debug!("compressed: {:?}...", &self.raw_data[.. self.raw_data.len().min(20)]);
             let mut data = Cow::Borrowed(&*self.raw_data);
             for filter in &self.info.filters {
                 data = match decode(&*data, filter) {
