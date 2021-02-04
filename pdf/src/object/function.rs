5ca2b8cded3599170f74b9064984cc7555324f68
Sebastian K <s3bk@protonmail.com>
2021-02-04 22:09:16 +0300

    add PS functions

--- pdf/src/object/function.rs
+++ pdf/src/object/function.rs
@@ -35,6 +35,7 @@ pub enum Function {
     Interpolated(Vec<InterpolatedFunctionDim>),
     Stiching,
     Calculator,
+    PostScript(PsFunc),
 }
 impl Function {
     pub fn apply(&self, x: f32, out: &mut [f32]) {
@@ -44,6 +45,7 @@ impl Function {
                     *y = f.apply(x);
                 }
             }
+            Function::PostScript(ref func) => func.exec(x, out).unwrap(),
             _ => panic!("unimplemted function {:?}", self)
         }
     }
@@ -52,9 +54,9 @@ impl Object for Function {
     fn serialize<W: io::Write>(&self, _out: &mut W) -> Result<()> {
         unimplemented!()
     }
-    fn from_primitive(p: Primitive, resolve: &impl Resolve) -> Result<Self> {
+    fn from_dict(dict: Dictionary, resolve: &impl Resolve) -> Result<Self> {
         use std::f32::INFINITY;
-        let raw = RawFunction::from_primitive(p, resolve)?;
+        let raw = RawFunction::from_dict(dict, resolve)?;
         match raw.function_type {
             2 => {
                 let f2 = Function2::from_dict(raw.other, resolve)?;
@@ -87,6 +89,26 @@ impl Object for Function {
             }
         }
     }
+    fn from_primitive(p: Primitive, resolve: &impl Resolve) -> Result<Self> {
+        dbg!(&p);
+        match p {
+            Primitive::Dictionary(dict) => Self::from_dict(dict, resolve),
+            Primitive::Stream(s) => {
+                match s.info.get("FunctionType") {
+                    Some(Primitive::Integer(4)) => {},
+                    _ => panic!()
+                }
+                let stream = Stream::<()>::from_stream(s, resolve)?;
+                let data = stream.decode()?;
+                let s = std::str::from_utf8(&*data)?;
+                println!("{}", s);
+                let func = PsFunc::parse(s)?;
+                Ok(Function::PostScript(func))
+            },
+            Primitive::Reference(r) => Self::from_primitive(resolve.resolve(r)?, resolve),
+            _ => unimplemented!()
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -102,5 +124,74 @@ impl InterpolatedFunctionDim {
         let y = self.c0 + x.powf(self.exponent) * (self.c1 - self.c0);
         let (y0, y1) = self.output_range;
         y.min(y1).max(y0)
+    }
+}
+
+#[derive(Debug)]
+pub enum PostScriptError {
+    StackUnderflow,
+    IncorrectStackSize
+}
+#[derive(Debug)]
+pub struct PsFunc {
+    pub ops: Vec<PsOp>
+}
+
+macro_rules! op {
+    ($stack:ident; $($v:ident),* => $($e:expr),*) => ( {
+        $(let $v = $stack.pop().ok_or(PostScriptError::StackUnderflow)?;)*
+        $($stack.push($e);)*
+    } )
+}
+
+impl PsFunc {
+    pub fn exec(&self, input: f32, output: &mut [f32]) -> Result<(), PostScriptError> {
+        let mut stack = Vec::with_capacity(10);
+        stack.push(input);
+        for &op in &self.ops {
+            match op {
+                PsOp::Value(v) => stack.push(v),
+                PsOp::Dup => op!(stack; v => v, v),
+                PsOp::Exch => op!(stack; a, b => a, b),
+                PsOp::Add => op!(stack; a, b => a + b),
+                PsOp::Mul => op!(stack; a, b => a * b),
+                PsOp::Abs => op!(stack; a => a.abs()),
+            }
+        }
+        output.copy_from_slice(&stack);
+        Ok(())
+    }
+    pub fn parse(s: &str) -> Result<Self, PdfError> {
+        let start = s.find("{").ok_or(PdfError::PostScriptParse)?;
+        let end = s.rfind("}").ok_or(PdfError::PostScriptParse)?;
+
+        let ops: Result<Vec<_>, _> = s[start + 1 .. end].split_ascii_whitespace().map(|p| PsOp::parse(p).ok_or(PdfError::PostScriptParse)).collect();
+        Ok(PsFunc { ops: ops? })
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum PsOp {
+    Value(f32),
+    Add,
+    Abs,
+    Mul,
+    Dup,
+    Exch,
+}
+impl PsOp {
+    pub fn parse(s: &str) -> Option<Self> {
+        if let Ok(f) = s.parse() {
+            Some(PsOp::Value(f))
+        } else {
+            Some(match s {
+                "add" => PsOp::Add,
+                "abs" => PsOp::Abs,
+                "mul" => PsOp::Mul,
+                "dup" => PsOp::Dup,
+                "exch" => PsOp::Exch,
+                _ => return None
+            })
+        }
     }
 }
