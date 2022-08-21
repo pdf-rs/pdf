18eb8e9acfb764fbc480a7dd751d3a73c3c32b8b
Eric Marsden <eric.marsden@foncsi.org>
2022-08-21 10:10:59 +0200

    Fix parsing of NameTree<Primitive> type.
    Resolves an error encountered with file sample01.pdf at
    https://github.com/sambitdash/PDFTest/tree/master/DigSig
    
    Try { file: "pdf/src/file.rs", line: 324, column: 23, context: Context([]), source: FromPrimitive { typ: "RcRef < Catalog >", field: "root", source: Shared { source: FromPrimitive { typ: "Option < MaybeRef < NameDictionary > >", field: "names", source: Shared { source: FromPrimitive { typ: "Option < NameTree < Primitive > >", field: "ids", source: UnexpectedPrimitive { expected: "String", found: "Reference" } } } } } } }

--- pdf/src/object/types.rs
+++ pdf/src/object/types.rs
@@ -413,7 +413,7 @@ impl ObjectWrite for Pattern {
         match self {
             Pattern::Dict(ref d) => d.to_primitive(update),
             Pattern::Stream(ref d, ref ops) => {
-                let data = serialize_ops(&ops)?;
+                let data = serialize_ops(ops)?;
                 let stream = Stream::new_with_filters(d.clone(), data, vec![]);
                 stream.to_primitive(update)
             }
@@ -926,7 +926,7 @@ impl<T: Object+DataSize> NameTree<T> {
 impl<T: Object> Object for NameTree<T> {
     fn from_primitive(p: Primitive, resolve: &impl Resolve) -> Result<Self> {
         let mut dict = t!(p.resolve(resolve)?.into_dictionary());
-        
+
         // Quite long function..=
         let limits = match dict.remove("Limits") {
             Some(limits) => {
@@ -959,7 +959,7 @@ impl<T: Object> Object for NameTree<T> {
                 let names = names.resolve(resolve)?.into_array()?;
                 let mut new_names = Vec::new();
                 for pair in names.chunks(2) {
-                    let name = pair[0].clone().into_string()?;
+                    let name = pair[0].clone().resolve(resolve)?.into_string()?;
                     let value = t!(T::from_primitive(pair[1].clone(), resolve));
                     new_names.push((name, value));
                 }
@@ -1035,7 +1035,7 @@ impl Dest {
                     ref p => return Err(PdfError::UnexpectedPrimitive { expected: "Number | Integer | Null", found: p.get_debug_name() }),
                 },
                 zoom: match array.get(4) {
-                    Some(&Primitive::Null) => 0.0,
+                    Some(Primitive::Null) => 0.0,
                     Some(&Primitive::Integer(n)) => n as f32,
                     Some(&Primitive::Number(f)) => f,
                     Some(ref p) => return Err(PdfError::UnexpectedPrimitive { expected: "Number | Integer | Null", found: p.get_debug_name() }),
