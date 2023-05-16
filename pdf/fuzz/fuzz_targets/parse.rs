548c4048d9b7e92950f06f7bce6278f84c2d3208
toka <tokazerkje@outlook.com>
2023-05-16 13:34:37 +0200

    upd

--- pdf/fuzz/fuzz_targets/parse.rs
+++ pdf/fuzz/fuzz_targets/parse.rs
@@ -1,8 +1,15 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: &[u8]| {
-    if let Ok(p) = pdf::file::File::from_data(data) {
-        for _ in p.pages() {}
+fn harness(data: &[u8]) {
+    if let Ok(file) = pdf::file::FileOptions::cached().load(data) {
+        for idx in 0..file.num_pages() {
+            let _ = file.get_page(idx);
+        }
     }
+    Ok(())
+}
+
+fuzz_target!(|data: &[u8]| {
+    let _ = harness(data);
 });
